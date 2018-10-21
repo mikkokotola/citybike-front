79bcdb15593d405c73a772d31d2b6e7dc7129b81
Mikko Kotola <mikko.kotola@iki.fi>
2018-10-21 15:12:24 +0300

    Updated php-fake.

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<style>
+
+  .line {
+    fill: none;
+    stroke: steelblue;
+    stroke-width: 1.5px;
+  }
+  
+  </style>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Trend Chart (Area + Line)</title>
+    <link rel="stylesheet" href="js-src/app.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+    <script type='text/javascript' src="env.js"></script>
+    <script src="//d3js.org/d3.v4.min.js"></script>
+  </head>
+
+  <body>
+
+      <div class="nav">
+        <p>CityBikes</p>
+        <p id="nav-date"></p>
+      </div>
+      
+      <div id="map"></div>
+
+      <div id="hidden-stash"> 
+        <div id="content">
+          <svg id="station-plot" width="300" height="200"></svg>
+          <div class="dialog">
+            <a href="#" onclick="hidePopup()" class="close-thin"></a>
+          </div>
+        </div>
+      </div>
+  </body>
+
+  <script type='text/javascript' src="js-src/popup.js"></script>
+  <script type='text/javascript' src="js-src/map.js"></script>
+  <script type='text/javascript' src="js-src/markers.js"></script>
+  <script type='text/javascript' src="js-src/maps_apikey.js"></script>
+  <script type='text/javascript' src="js-src/plotter.js"></script>
+</html>
